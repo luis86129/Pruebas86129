29b97910380010574d06f7cbeb3accdbea7bfbec
Luis Mendoza <luismendoza86129@gmail.com>
2026-05-14 18:28:06 -0500

    test ingreso proyecto java
    Intentare ingresar un proyecto diseñado en java

---
+++ Proyecto_Java/src/main/java/com/mycompany/proyecto_java/Proyecto_Java.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_java;
+
+/**
+ *
+ * @author Luis Mendoza
+ */
+public class Proyecto_Java {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
