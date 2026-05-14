d071e0d425d83f4025ed23106bdf090dc23182a0
Luis Mendoza <luismendoza86129@gmail.com>
2026-05-14 18:30:52 -0500

    Editando un print
    Edite un print en java

--- Proyecto_Java/src/main/java/com/mycompany/proyecto_java/Proyecto_Java.java
+++ Proyecto_Java/src/main/java/com/mycompany/proyecto_java/Proyecto_Java.java
@@ -11,6 +11,6 @@ package com.mycompany.proyecto_java;
 public class Proyecto_Java {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(" Esta es una pruebaaaa !!! ");
     }
 }
