7ea8300dbef07b426e3aee8a4a34b41faa554fae
Luis Mendoza <luismendoza86129@gmail.com>
2026-05-14 18:36:31 -0500

    a

--- Proyecto_Java/src/main/java/com/mycompany/proyecto_java/Proyecto_Java.java
+++ Proyecto_Java/src/main/java/com/mycompany/proyecto_java/Proyecto_Java.java
@@ -12,5 +12,10 @@ public class Proyecto_Java {
 
     public static void main(String[] args) {
         System.out.println(" Esta es una pruebaaaa !!! ");
+        int x = 10;
+        int y = 20;
+        int suma = x + y;
+        System.out.println(" La suma de x e y es: " + suma);
+        
     }
 }
