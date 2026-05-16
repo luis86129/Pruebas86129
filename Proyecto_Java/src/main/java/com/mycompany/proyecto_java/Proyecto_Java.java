6ec8f4ddad54bad3016200f587dab8043a598a38
Luis Mendoza <luismendoza86129@gmail.com>
2026-05-15 21:20:02 -0500

    agrege la linea 21 y comente la 14

--- Proyecto_Java/src/main/java/com/mycompany/proyecto_java/Proyecto_Java.java
+++ Proyecto_Java/src/main/java/com/mycompany/proyecto_java/Proyecto_Java.java
@@ -11,12 +11,13 @@ package com.mycompany.proyecto_java;
 public class Proyecto_Java {
 
     public static void main(String[] args) {
-        new panel1().setVisible(true);
+        //new panel1().setVisible(true);
         System.out.println(" Esta es una pruebaaaa !!! ");
         int x = 10;
         int y = 20;
         int suma = x + y;
         System.out.println(" La suma de x e y es: " + suma);
         System.out.println(" Hola de nuevo, esta es otra prueba !!! ");
+        System.out.println(" Esta es la linea 21 ");
     }
 }
